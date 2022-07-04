344b6dd5a0ac59aaf41d09f9fa30e8ac3b8a90ff
Song0129 <Song_3191@163.com>
2022-07-04 18:05:45 +0800

    update

---
+++ 02-SFC/app.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<div>hi,app</div>
+		<p>my vue-cli-service-global</p>
+		<p>count: {{ count }}</p>
+		<button @click="handleClick">add</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				count: 1,
+			};
+		},
+		methods: {
+			handleClick() {
+				console.log("button click");
+				this.count++;
+			},
+		},
+	};
+</script>
+
+<style lang="scss" scoped></style>
