5b0195a5ed868dfec423c68b1c42380d9f6be91a
Song0129 <Song_3191@163.com>
2022-07-05 16:53:38 +0800

    update

---
+++ 04-todo-list/src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+	<div>
+		<input type="text" @keyup.enter="addTodo" v-model="newTodo" />
+		<div>
+			<ul>
+				<li v-for="item in todoList" :key="item.id">{{ item.text }}</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	let id = 1;
+	function createId() {
+		return id++;
+	}
+	export default {
+		data() {
+			return {
+				newTodo: "",
+				todoList: [],
+			};
+		},
+		methods: {
+			addTodo() {
+				console.log(this.newTodo);
+				this.todoList.push({
+					id: createId(),
+					text: this.newTodo,
+				});
+				this.newTodo = "";
+			},
+		},
+	};
+</script>
+
+<style lang="less" scoped>
+	* {
+		margin: 0;
+		padding: 0;
+	}
+</style>
