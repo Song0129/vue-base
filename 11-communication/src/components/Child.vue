ed48979dc269821d4ad5788231bf85ed256b2d95
Song0129 <Song_3191@163.com>
2022-07-14 15:10:16 +0800

    update

---
+++ 11-communication/src/components/Child.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		Child
+		<div>title:{{ title }}---childMsg: {{ childMsg }}</div>
+		<button @click="toParent">To Parent</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		// input
+		props: ["title"],
+		data() {
+			return {
+				childMsg: "",
+			};
+		},
+		methods: {
+			toParent() {
+				this.$emit("to-parent");
+			},
+			setMsg(val) {
+				this.childMsg = val;
+			},
+		},
+	};
+</script>
+
+<style lang="less" scoped></style>
---
+++ 11-communication/src/components/Parent.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		Parent
+		<div>msg:{{ msg }}</div>
+		<Child ref="child" title="this is parent title" @to-parent="handleToParent"></Child>
+		<button @click="handleGetChildByRef">get child by ref</button>
+	</div>
+</template>
+
+<script>
+	import Child from "./Child.vue";
+	export default {
+		components: {
+			Child,
+		},
+		data() {
+			return {
+				msg: "heihei",
+			};
+		},
+		methods: {
+			handleToParent() {
+				console.log("to parent in parent");
+				this.msg = "haha";
+			},
+			handleGetChildByRef() {
+				console.log("get child by ref");
+				console.log(this.$refs);
+				this.$refs.child.setMsg("heiheihei - from parent");
+			},
+		},
+	};
+</script>
+
+<style lang="less" scoped></style>
