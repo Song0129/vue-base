31151069f59f9b9461c254e0025ed4e8d00a9802
Song0129 <Song_3191@163.com>
2022-08-05 17:14:05 +0800

    update

--- 17-learn-render/src/main.js
+++ 17-learn-render/src/main.js
@@ -1,11 +1,34 @@
-import { createApp } from "vue";
+// import { createApp, createRenderer } from "vue";
+import { createRenderer } from "vue";
 import App from "./App.vue";
 
 // template -> render -> vnode(element) -> mountElment -> element -> append()
 
-console.log(App);
-console.log(App.render());
+// console.log(App);
+// console.log(App.render());
 
 // type children props
 
-createApp(App).mount("#app");
+const renderer = createRenderer({
+	createElement(type) {
+		console.log(type);
+		const element = document.createElement(type);
+		return element;
+	},
+	insert(el, parent) {
+		console.log(el);
+		console.log(parent);
+
+		parent.append(el);
+	},
+	setElementText(node, text) {
+		console.log(node);
+		console.log(text);
+		node.append(document.createTextNode(text));
+	},
+});
+console.log(renderer);
+
+renderer.createApp(App).mount(document.querySelector("#app"));
+
+// createApp(App).mount("#app");
