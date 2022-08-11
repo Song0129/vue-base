d8e74f5af9343b0fbd4e4679eedf514381819a55
Song0129 <Song_3191@163.com>
2022-08-11 18:41:19 +0800

    update

--- 18-learn-pixijs/src/main.js
+++ 18-learn-pixijs/src/main.js
@@ -1,6 +1,7 @@
 // import * as PIXI from "pixi.js";
 
-import { Application } from "pixi.js";
+import { Application, Graphics, Sprite, Texture, Text, Container } from "pixi.js";
+import logo from "./assets/logo.png";
 
 console.log(Application);
 const game = new Application({
@@ -8,4 +9,38 @@ const game = new Application({
 	height: 500,
 });
 
-console.log(game);
+const box1 = new Container();
+game.stage.addChild(box1);
+
+const rect = new Graphics();
+rect.beginFill(0xff0000);
+rect.drawRect(0, 0, 50, 50);
+rect.endFill();
+
+rect.interactive = true;
+rect.on("pointertap", () => {
+	console.log("click");
+	game.ticker.remove(handleTicker);
+});
+
+game.stage.addChild(rect);
+
+const img = new Sprite();
+// img -> url
+img.texture = Texture.from(logo);
+box1.addChild(img);
+
+const text = new Text("hello pixijs", { fill: "red" });
+box1.addChild(text);
+
+box1.x = 100;
+box1.y = 100;
+
+function handleTicker() {
+	console.log("add");
+	box1.x++;
+}
+
+game.ticker.add(handleTicker);
+
+document.body.append(game.view);
