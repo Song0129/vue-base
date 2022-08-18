ce83b98ba919871c6b5b247133379735a3c6744c
Song0129 <Song_3191@163.com>
2022-08-18 17:11:27 +0800

    update

--- 22-mini-vue/core/reactivity.js
+++ 22-mini-vue/core/reactivity.js
@@ -36,3 +36,37 @@ export function effectWatch(fn) {
 	fn();
 	currentEffect = null;
 }
+
+const targetsMap = new Map();
+export function reactive(raw) {
+	return new Proxy(raw, {
+		get(target, key) {
+			console.log(target, key);
+			let dep = getDep(target, key);
+			dep.depend();
+
+			return Reflect.get(target, key);
+		},
+		set(target, key, value) {
+			let dep = getDep(target, key);
+			const result = Reflect.set(target, key, value);
+			dep.notice();
+			return result;
+		},
+	});
+}
+
+function getDep(raw, key) {
+	let depsMap = targetsMap.get(raw);
+	if (!depsMap) {
+		depsMap = new Map();
+		targetsMap.set(raw, depsMap);
+	}
+
+	let dep = depsMap.get(key);
+	if (!dep) {
+		dep = new Dep();
+		depsMap.set(key, dep);
+	}
+	return dep;
+}
--- 22-mini-vue/main.js
+++ 22-mini-vue/main.js
@@ -13,14 +13,26 @@
 // // set操作  触发依赖
 // a.value = 20;
 
-import { Dep, effectWatch } from "./core/index.js";
+import { Dep, effectWatch, reactive } from "./core/index.js";
 
-const a = new Dep(10);
-let b = 0;
+// const a = new Dep(10);
+// let b = 0;
 
+// effectWatch(() => {
+// 	b = a.value + 10;
+// 	console.log(b);
+// });
+
+// a.value = 20;
+
+// {} -> reactive
+
+const user = reactive({ age: 10 });
+
+let nextAge = 0;
 effectWatch(() => {
-	b = a.value + 10;
-	console.log(b);
+	nextAge = user.age + 1;
+	console.log(nextAge);
 });
 
-a.value = 20;
+user.age++;
