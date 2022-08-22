f7a45f043a5dde5e130822c236dcc4ce980f8280
Song0129 <Song_3191@163.com>
2022-08-22 14:45:58 +0800

    update

--- 22-mini-vue/App.js
+++ 22-mini-vue/App.js
@@ -1,25 +1,42 @@
 import { reactive, h } from "./core/index.js";
+window.h = h;
 export default {
 	// template -> render
 	render(context) {
 		// ui
 		// 1. 每次删除所有节点 -> 优化
 		// 2. 跨平台的问题
-
 		// const element = document.createElement("div");
 		// const text = document.createTextNode("hello,");
 		// const text1 = document.createTextNode(context.obj.count);
 		// element.append(text);
 		// element.append(text1);
-
 		// return element;
-
-		return h("div", { id: "box", class: "test" }, [h("p", {}, "hello"), h("p", {}, String(context.obj.count))]);
+		// return h("div", { id: "box", class: "test" }, [h("p", {}, "hello"), h("p", {}, String(context.obj.count))]);
 		// return h("div", { id: "box", class: "test" }, "hello");
+		// 1. test tag
+		// return h(context.obj.tag, {}, "1");
+		// 2. test props add/update
+		// return h("div", context.obj.props, "1");
+		// 3. test props remove
+		// return h("div", context.obj.props, "1");
+		// 1. test oldChildren -> string | newChildren -> string
+		// return h("div", {}, String(context.obj.count));
+		// 2. test oldChildren -> array | newChildren -> string
+		// return h("div", {}, context.obj.children);
+		// 3. test oldChildren -> string | newChildren -> children
+		// return h("div", {}, context.obj.children);
+		// 4. test oldChildren -> array | newChildren -> children
+		//  4.1 new > old  | new < old
+		return h("div", {}, context.obj.children);
 	},
 	setup() {
 		const obj = reactive({
 			count: 1,
+			tag: "div",
+			props: { a: "a", b: "b" },
+			children: [h("p", {}, "1"), h("p", {}, "2")],
+			// children: "a",
 		});
 
 		window.obj = obj;
--- 22-mini-vue/core/renderer.js
+++ 22-mini-vue/core/renderer.js
@@ -5,7 +5,11 @@ function createElement(tag) {
 }
 
 function patchProps(el, key, prevVal, nextVal) {
-	el.setAttribute(key, nextVal);
+	if (nextVal === null) {
+		el.removeAttribute(key);
+	} else {
+		el.setAttribute(key, nextVal);
+	}
 }
 
 function insert(el, parent) {
@@ -16,7 +20,7 @@ function createTextNode(text) {
 	return document.createTextNode(text);
 }
 
-// 挂载
+// 挂载 vdom -> dom
 export function mountElement(vnode, container) {
 	const { tag, props, children } = vnode;
 	// tag
@@ -46,4 +50,80 @@ export function mountElement(vnode, container) {
 // n2 -> newvNode
 export function diff(n1, n2) {
 	console.log("diff", n1, n2);
+
+	if (n1.tag !== n2.tag) {
+		// 1. tag
+		n1.el.replaceWith(createElement(n2.tag));
+	} else {
+		// 2. props
+		//  2.1. new {a,b} -> old {a} -> add b
+		//  2.2. new {a} -> old {a,b} -> remove b
+		const { props: oldProps } = n1;
+		const { props: newProps } = n2;
+		const el = (n2.el = n1.el);
+		if (newProps) {
+			Object.keys(newProps).forEach(key => {
+				const newVal = newProps[key];
+				const oldVal = oldProps[key];
+
+				if (newVal !== oldVal) {
+					patchProps(el, key, oldVal, newVal);
+				}
+			});
+		}
+
+		if (oldProps) {
+			Object.keys(oldProps).forEach(key => {
+				if (!(key in newProps)) {
+					patchProps(el, key, oldProps[key], null);
+				}
+			});
+		}
+
+		// 3. children
+		//  3.1 newChildren -> string (oldChildren -> string | oldChildren -> array)
+		//  3.2 newChildren -> array (oldChildren -> string | oldChildren -> array)
+
+		const { children: oldChildren } = n1;
+		const { children: newChildren } = n2;
+		if (typeof newChildren === "string") {
+			if (typeof oldChildren === "string") {
+				if (newChildren !== oldChildren) {
+					el.textContent = newChildren;
+				}
+			} else if (Array.isArray(oldChildren)) {
+				el.textContent = newChildren;
+			}
+		} else if (Array.isArray(newChildren)) {
+			if (typeof oldChildren === "string") {
+				el.innerText = "";
+				newChildren.forEach(v => {
+					mountElement(v, el);
+				});
+			} else if (Array.isArray(oldChildren)) {
+				// 暴力对比 无法处理 old -> [a,b,c] | new -> [a,c,b]
+				// 1. 公共部分依次对比
+				const length = Math.min(oldChildren.length, newChildren.length);
+				for (let i = 0; i < length; i++) {
+					const newVNode = newChildren[i];
+					const oldVNode = oldChildren[i];
+					diff(oldVNode, newVNode);
+				}
+				// 2. new > old -> add  [a,b] -> [a,b,c]
+				if (newChildren.length > oldChildren.length) {
+					for (let i = length; i < newChildren.length; i++) {
+						const VNode = newChildren[i];
+						mountElement(VNode, el);
+					}
+				}
+				// 3. new < old -> remove  [a,b,c] -> [a,b]
+				if (newChildren.length < oldChildren.length) {
+					for (let i = length; i < oldChildren.length; i++) {
+						const oldVNode = oldChildren[i];
+						el.removeChild(oldVNode.el);
+					}
+				}
+			}
+		}
+	}
 }
